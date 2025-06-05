00f4b2032afe4d86ce251888363a7b60a188c31e
Pratham <pratham.ahire@bitbybitsolutions.co.in>
2025-06-05 18:43:47 +0530

    Initial commit

---
+++ frontend/Task-Manager/src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import Signup from './pages/Auth/Signup'
+import PrivateRoute from './routes/PrivateRoute'
+import Dashboard from './pages/Admin/Dashboard'
+import ManageTask from './pages/Admin/ManageTask'
+import CreateTask from './pages/Admin/CreateTask'
+import ManageUsers from './pages/Admin/ManageUsers'
+import Dashboard from './pages/User/Dashboard'
+
+
+function App() {
+  return (
+    <div >
+      <Router>
+        <Routes>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signUp' element={<Signup/>}/>
+
+          {/* Admin Routes*/}
+
+          <Route element={<PrivateRoute allowedRoles={('admin')}/>}>
+          <Route path='/admin/dashboard' element={<Dashboard/>}/>
+           <Route path='/admin/tasks' element={<ManageTask/>}/>
+            <Route path='/admin/create-task' element={<CreateTask/>}/>
+             <Route path='/admin/users' element={<ManageUsers/>}/>
+          </Route>
+
+            {/* User Routes*/}
+
+          <Route element={<PrivateRoute allowedRoles={('user')}/>}>
+          <Route path='/user/dashboard' element={<Dashboard/>}/>
+          </Route>
+
+
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
